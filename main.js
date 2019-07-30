bedcb8e5eb46cedeb69afd1a2d1430c8923f8fd9
AbdoWahba <abdo.tw20154@gmail.com>
2019-07-30 22:10:20 +0200

    adding local and online server to fetch from them

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 function performGetRequest1() {
   var getResult = document.getElementById("getresult1");
   getResult.innerHTML = "";
-  axios.get("http://localhost:3000/todos").then(function(responce) {
-    getResult.innerHTML =
-      "<pre>" + JSON.stringify(responce.data, null, "\t") + "</pre>";
-    console.log(JSON.stringify(responce.data, null, "\t"));
-  });
+  axios
+    .get("http://localhost:3000/todos")
+    .then(function(responce) {
+      getResult.innerHTML =
+        "<pre>" + JSON.stringify(responce.data, null, "\t") + "</pre>";
+    })
+    .catch(function(err) {
+      axios
+        .get("https://jsonplaceholder.typicode.com/todos")
+        .then(function(response) {
+          getResult.innerHTML =
+            "<pre>" + JSON.stringify(response.data, null, "\t") + "</pre>";
+          console.log(JSON.stringify(response.data, null, "\t"));
+        })
+        .catch(function(err) {
+          getResult.innerHTML =
+            "<pre>" + JSON.stringify(err, null, "\t") + "</pre>";
+        });
+    });
 }
