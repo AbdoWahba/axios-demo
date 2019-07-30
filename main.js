f5027cb06db3121409eca379a76913db371f74ca
AbdoWahba <abdo.tw20154@gmail.com>
2019-07-30 22:35:16 +0200

    impemintig get request with parameter function

--- main.js
+++ main.js
@@ -21,3 +21,31 @@ function performGetRequest1() {
         });
     });
 }
+
+function performGetRequest2() {
+  var getResult = document.getElementById("getresult2");
+  getResult.innerHTML = "";
+  axios
+    .get(
+      "http://localhost:3000/todos/" + document.getElementById("todoid").value
+    )
+    .then(function(responce) {
+      getResult.innerHTML =
+        "<pre>" + JSON.stringify(responce.data, null, "\t") + "</pre>";
+    })
+    .catch(function(err) {
+      axios
+        .get("https://jsonplaceholder.typicode.com/todos", {
+          params: { id: document.getElementById("todoid").value }
+        })
+        .then(function(response) {
+          getResult.innerHTML =
+            "<pre>" + JSON.stringify(response.data, null, "\t") + "</pre>";
+          console.log(JSON.stringify(response.data, null, "\t"));
+        })
+        .catch(function(err) {
+          getResult.innerHTML =
+            "<pre>" + JSON.stringify(err, null, "\t") + "</pre>";
+        });
+    });
+}
